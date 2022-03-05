7142d353248df8ffa6ea15973bedad70769c08ef
jon <marek1990kaminski@gmail.com>
2022-03-05 18:56:10 +0100

    no appbar no drawer no footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,91 +2,11 @@
 
   <v-app dark>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"/>
-          </v-list-item-content>
-
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
-
-      <v-toolbar-title v-text="title"/>
-
-      <v-spacer/>
-
-      <v-tooltip
-        v-if="!account"
-        bottom
-      >
-        <template #activator="{ on, attrs }">
-          <v-icon
-            target="_blank"
-            color="blue lighten-1"
-            v-bind="attrs"
-            v-on="on"
-            @click="signIn"
-          >
-            mdi-microsoft-azure
-          </v-icon>
-        </template>
-        <span>login through Azure</span>
-      </v-tooltip>
-
-      <v-tooltip
-        v-if="!!account"
-        bottom
-      >
-        <template #activator="{ on, attrs }">
-          <v-icon
-            target="_blank"
-            color="red lighten-1"
-            v-bind="attrs"
-            v-on="on"
-            @click="signOut"
-          >
-            mdi-logout
-          </v-icon>
-        </template>
-        <span>logout</span>
-      </v-tooltip>
-
-    </v-app-bar>
-
     <v-main>
       <v-container>
         <Nuxt/>
       </v-container>
     </v-main>
-
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }} by MK</span>
-    </v-footer>
 
   </v-app>
 
